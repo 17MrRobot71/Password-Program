ae341fb5fbb4224098c3dcfe19eabde6cb24b8b4
17MrRobot71 <114177706+17MrRobot71@users.noreply.github.com>
2023-05-26 12:40:15 +0300

    Добавьте файлы проекта.

---
+++ Список завданнь/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Список_завданнь
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(314, 230);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Список паролів";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(524, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Видалення";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(467, 222);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(467, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(300, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(524, 103);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(172, 49);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Додавання";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(321, 103);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(172, 49);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Рандом пароль";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(344, 66);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(93, 20);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(264, 332);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(86, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Зберегти";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(372, 332);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(86, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Відкрити";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(264, 295);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(205, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Список завданнь/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+using static Список_завданнь.Form1;
+
+namespace Список_завданнь
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Password
+        {
+            public string password;
+            public Password(string password)
+            {
+                this.password = password;
+            }
+        }
+        public List<Password> passw = new List<Password>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            foreach (Password pass in passw)
+            {
+                richTextBox1.AppendText(pass.password + Environment.NewLine);
+            }
+        }
+        public string[] arr = { "a", "b", "c", "1", "2", "3", "x", "y", "z", "4" };
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                passw.Add(new Password("Text"));
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string newpass = textBox2.Text;
+            passw.Add(new Password(newpass));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            richTextBox1.Clear();
+            int k = (int)numericUpDown1.Value;
+            if (k == 0)
+            {
+                MessageBox.Show("Довжина паролю повинна бути більше за нуль");
+            }
+            else
+            {
+
+
+                for (int i = 0; i < k; i++)
+                {
+
+
+                    int x = rand.Next(0, arr.Length);
+                    richTextBox1.Text += arr[x];
+
+
+                }
+                passw.Add(new Password(richTextBox1.Text));
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            string newpass = textBox1.Text;
+            List<Password> passwordsToRemove = passw.Where(p => p.password == newpass).ToList();
+            foreach (Password passwordToRemove in passwordsToRemove)
+            {
+                passw.Remove(passwordToRemove);
+            }
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            string tofile = textBox3.Text;
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(tofile))
+            {
+               
+
+
+                    foreach (Password password in passw)
+                    {
+                        writer.WriteLine(password.password);
+                    }
+               
+                   
+            }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Виникла помилка {ex.Message}");
+            }
+        }
+    }
+
+    }
+
+    
+
